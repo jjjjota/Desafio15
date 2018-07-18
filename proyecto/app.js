a3e607beda0f6a1d41c1b15227077a599521ef01
jjjjota <jajopia@uc.cl>
2018-07-18 12:44:54 -0400

    :baby_chick: Cleaning the code

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,4 +1,9 @@
-// Variables datos
+// Variables de selectores
+var select = $("#select"),
+    resumen = $("#resumen"),
+    imagen = $(".img-responsive");
+
+// Variables con datos
 var coords = {
  	isla: {
  		lat: -27.112723,
@@ -23,41 +28,36 @@ var image = {
     'rain':'https://icons.wxug.com/i/c/v4/rain.svg'
   }
 
-
-
 // Variables para API Maps
-var map = null;
-var marker = null;
+var map = null,
+    marker = null;
 
+// Varibales para API Dark Sky
+var url = 'https://api.darksky.net/forecast/',
+    key = 'cb080bb90bde9e641e6c42cf6f26913e/',
+    queryParams = ['exclude=[minutely,hourly,daily,alerts,flags]', 'lang=es', 'units=auto'];
+    // var proxy = 'https://crossorigin.me/';
+
+// Inicializar el mapa
 function initMap() {
 	var location = null;
+
+  // Crear objeto Map
 	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 11,
 		center: location
 	});
 
+  // Crear objeto Marker
 	marker = new google.maps.Marker({
 		position: location,
 		map: map
 	});
 }
 
-// var proxy = 'https://crossorigin.me/';
-
-// var urlMaps = 'https://api.darksky.net/forecast/',
-//     keyMaps = 'e193913a8a2bab291b8999fae03fc8b7/',
-//     queryParamsMaps = ['exclude=[minutely,flags]', 'lang=es', 'units=auto'];
-
-
-// Varibales para API Dark Sky
-var urlSky = 'https://api.darksky.net/forecast/',
-    keySky = 'cb080bb90bde9e641e6c42cf6f26913e/',
-    queryParamsSky = ['exclude=[minutely,hourly,daily,alerts,flags]', 'lang=es', 'units=auto'];
-
-
 // EVENTO change en #select
-$('#select').on('change', function() {
-  // Inicializar el mapa
+select.on('change', function() {
+  // Setear el mapa con datos del #select
 	map.setCenter(coords[$(this).val()]);
 	marker.setMap(null);
 	marker = new google.maps.Marker({
@@ -65,27 +65,15 @@ $('#select').on('change', function() {
 		map: map
 	});
 
-  // REQUEST a API Maps
-	// $.ajax({
-	// 	url: proxy + urlMaps + keyMaps + coords[$(this).val()].lat + ',' + coords[$(this).val()].lng + '?' + queryParamsMaps[0] + '&' + queryParamsMaps[1] + '&' + queryParamsMaps[2],
-	// 	method: 'GET',
-	// 	xhrFields: {cors: false}
-  // }).then(function() {
-	// });
 
   // REQUEST a API Dark Sky
   $.ajax({
-    url: urlSky + keySky + coords[$(this).val()].lat + ',' + coords[$(this).val()].lng + '?' + queryParamsSky[0] + '&' + queryParamsSky[1] + '&' + queryParamsSky[2],
+    url: url + key + coords[$(this).val()].lat + ',' + coords[$(this).val()].lng + '?' + queryParams[0] + '&' + queryParams[1] + '&' + queryParams[2],
     method: 'GET',
-
-    dataType: "jsonp",
-    headers: {
-      "accept": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
+    xhrFields: {cors: false}
   }).then(function(data) {
-    $('#resumen').text(parseInt(data.currently.temperature) + '° ' + data.currently.summary);
-    $('.img-responsive').attr('src',image[data.currently.icon]);
+    resumen.text(parseInt(data.currently.temperature) + '° ' + data.currently.summary);
+    imagen.attr('src',image[data.currently.icon]);
   });
 
 });
